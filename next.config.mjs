5adadcd2747bc1fe248f10027745561e229c0f17
Md Hasibul Islam <135458912+Mdhasibulislamgit@users.noreply.github.com>
2025-03-16 10:30:18 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+   basePath: '', // No subdirectory
+  assetPrefix: './', // Ensures assets load correctly
   // Your Next.js configuration options
 };
 
